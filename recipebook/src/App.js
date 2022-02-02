d34e7606325aa3c887c0b6e24996d4d86888cf23
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-02-01 23:28:06 -0600

    not understanding why react-router-dom is not working

--- recipebook/src/App.js
+++ recipebook/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <ul>
+            <Link to="/">Home</Link>
+            <li>Log in</li>
+            <li>Search</li>
+          </ul>
+            <Route path='/'></Route>
+        </div>
       </header>
     </div>
   );
